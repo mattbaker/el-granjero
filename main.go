22c66170a5b61800621824b25f4739bc2d9eed9b
Matt Baker <mbaker.pdx@gmail.com>
2016-10-07 23:25:58 -0500

    trying to hit endpoint

--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	key := os.Getenv("DESTINY_KEY")
-	fmt.Printf("Key is: %s\n", key)
+	endpointURL := "http://www.bungie.net/Platform/Destiny/Explorer/Items/"
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("GET", endpointURL, nil)
+	req.Header.Add("X-API-Key", key)
+
+	fmt.Print(req.Header)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln("Failed to make request")
+	}
+
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
 }
