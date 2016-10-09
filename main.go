6998fb563df02a150c19c72b437e4ffb4303125e
Jamie Schlessinger <jamie@scripted.com>
2016-10-09 10:29:19 -0500

    inventories and shit

--- main.go
+++ main.go
@@ -23,12 +23,30 @@ type ProfileResponse struct {
 	Response []Profile
 }
 
+type InventoryResponse struct {
+	*BaseResponse
+	Response struct {
+		Data struct {
+			Items []InventoryItem
+		}
+	}
+}
+
 // Profile represents a profile
 type Profile struct {
 	IconPath       string
 	MembershipType int
 	MembershipID   string
 	DisplayName    string
+}
+
+type InventoryItem struct {
+	ItemHash       int
+	ItemID         string
+	Quantity       int
+	TransferStatus int
+	State          int
+	BucketHash     int
 }
 
 var client = &http.Client{}
@@ -49,6 +67,7 @@ func apiRequest(endpoint string) string {
 func findPsnMember(username string) (*Profile, error) {
 	endPoint := fmt.Sprintf("https://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/2/%s/", username)
 	responseString := apiRequest(endPoint)
+	// fmt.Printf("Member Response:\n%s\n", responseString)
 	res := ProfileResponse{}
 	json.Unmarshal([]byte(responseString), &res)
 	if len(res.Response) == 0 {
@@ -61,11 +80,25 @@ func getCharacters() {
 
 }
 
-func getInventory() {
+func getFullInventory(membershipID string) ([]InventoryItem, error) {
+	endPoint := fmt.Sprintf("https://www.bungie.net/Platform/Destiny/2/Account/%s/Items/", membershipID)
+	responseString := apiRequest(endPoint)
+	// fmt.Printf("Inventory Response:\n%s\n", responseString)
+	res := InventoryResponse{}
+	json.Unmarshal([]byte(responseString), &res)
+	if len(res.Response.Data.Items) == 0 {
+		return nil, fmt.Errorf("inventory not found for membership ID %s", membershipID)
+	}
+	return res.Response.Data.Items, nil
+}
+
+func getCharacterInventory() {
 	// http://www.bungie.net/platform/Destiny/[membershipType]/Account/[d]/Character/[g]/Inventory/
 }
 
 func main() {
 	profile, _ := findPsnMember("swigwillis")
-	fmt.Printf("%+v\n", profile)
+	fmt.Printf("MembershipID: %s\n", profile.MembershipID)
+	inventory, _ := getFullInventory(profile.MembershipID)
+	fmt.Printf("%+v\n", inventory)
 }
